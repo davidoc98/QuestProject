ebdea5b704a4b8cad8f49ac1606b98b087212a81
david <david@localhost.localdomain>
2020-09-17 18:08:43 +0300

    Всё настроил и начал собирать приложение

---
+++ QP/app/Http/Livewire/ProfileComponent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileComponent extends Component
+{
+    public $user, $name;
+
+    public function render()
+    {
+        $this->user = Auth::user();
+        return view('livewire.profile.component');
+    }
+}
---
+++ QP/resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div style="padding: 10px; background: grey;">Привет, это профиль</div>
+<livewire:profile-component>
+
+@endsection
